7e9845bfd0838ac576d89f967c449d451a698908
Ilona Brand <theilonabrand@gmail.com>
2016-01-30 00:22:49 -0500

    Potentially onlly plays rain when its raining. Need to test...

--- rainClient.js
+++ rainClient.js
@@ -2,13 +2,25 @@ var request = require('request');
 var apiKey = require('./config.js').apiKey;
 var zipcode = require('./config.js').zipcode;
 
+var player = require('play-sound')(opts = {});
+
 function getWeather() {
     request('http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+',US&APPID='+apiKey, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            console.log(body) // Print the resp
+            var weatherData = JSON.parse(body);
+            var weatherId = weatherData.weather[0].id;
+
+            console.log(weatherData) // Print the resp
+            if (weatherId >= 200 && weatherId < 600) {
+                player.play('./Audio/rainyMood.mp3', function(err){
+                    console.log('ERROR PLAYING MP3');
+                });
+            }
         }
     });
-    setTimeout(getWeather, 10000)
+
+    // Only GETs every 10 minutes since the audio loop is 12 minutes long
+    setTimeout(getWeather, 10000);
 }
 
 getWeather();
